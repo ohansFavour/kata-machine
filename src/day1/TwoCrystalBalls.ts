332002aea973aafa0ef3afc227f4d54df6291fa2
ohansFavour <fohanekwu@gmail.com>
2022-09-25 12:25:31 +0100

    Solve two crystall ball algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const stackLength = breaks.length;
+    const jumpInterval = Math.floor(Math.sqrt(stackLength));
+    let i = 0;
+    for (; i < stackLength; i += jumpInterval) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpInterval;
+    for (; i < stackLength; ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
