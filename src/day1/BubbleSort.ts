7430b5ea85990d467d0901e32f8d9a281b27eb56
ohansFavour <fohanekwu@gmail.com>
2022-09-25 14:41:49 +0100

    Solve Bubble sort algorithm

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    const arrLength = arr.length;
+    for (let i = 0; i < arrLength - 1; ++i) {
+        for (let j = 0; j < arrLength - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
